8c671766418e8b49e21dca3ef191e24504c05bcc
tango <tangofun@live.jp>
2020-12-10 12:45:11 -0300

    V0.0.1

---
+++ blogs/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
+
+class Blog(models.Model):
+    title = models.CharField(blank=False, null=False, max_length=150)
+    text = models.TextField(blank=True)
+    # temp = models.FileField(upload_to=user_directory_path)
+    # file will be saved to MEDIA_ROOT/uploads/2015/01/30
+    upload = models.FileField(blank=True, upload_to='uploads/%Y/%m/%d/')
+    created_datetime = models.DateTimeField(auto_now_add=True)
+    updated_datetime = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
---
+++ polls/urls.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      tango
+#
+# Created:     04/12/2020
+# Copyright:   (c) tango 2020
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+from django.urls import path
+
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
