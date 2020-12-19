731a74f7f93a8da0d3cdf82fa40dd75cad42f771
tango <tangofun@live.jp>
2020-12-19 13:19:59 -0300

    test Azure

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,8 +32,8 @@ SECRET_KEY = '=5ph&+-9cecu&%dt0l5o)(ps52$_6@^4@=#&-2r*3)dnws(ou='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
-ALLOWED_HOSTS = ['localhost', 'myblogpyapp']
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+# ALLOWED_HOSTS = ['localhost', 'myblogpyapp']
 
 
 # Application definition
