0f458c5a51a09537128d4e5fd6886237f2136cb6
tango <tangofun@live.jp>
2021-03-11 15:28:06 -0300

    add whitenoise

--- mysite/s3/utils.py
+++ mysite/s3/utils.py
@@ -2,5 +2,5 @@
 
 from storages.backends.s3boto3 import S3Boto3Storage
  
-def MediaRootS3BotoStorage(): return S3Boto3Storage(location='media')
-def StaticRootS3BotoStorage(): return S3Boto3Storage(location='static')
+def MediaRootS3BotoStorage(): return S3Boto3Storage(location='media_root')
+def StaticRootS3BotoStorage(): return S3Boto3Storage(location='static_root')
